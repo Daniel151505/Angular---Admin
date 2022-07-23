b2383c14307d06622a23804827f181c7d85c788c
Daniel151505 <daniel150520011@gmail.com>
2022-07-23 18:12:34 -0500

    @Input - Componente Incrementador

--- adminpro/src/app/components/incrementador/incrementador.component.ts
+++ adminpro/src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class IncrementadorComponent  {
 
-  progreso: number = 10;
+  @Input() progreso: number = 10;
 
   get getPorcentaje() {
     return `${ this.progreso }%`;
