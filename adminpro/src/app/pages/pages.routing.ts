712f8d8b731d2e1e58ad524b2b21298605f5ee63
Daniel151505 <daniel150520011@gmail.com>
2022-07-23 16:11:47 -0500

    Rutas a partir de un path específico

--- adminpro/src/app/pages/pages.routing.ts
+++ adminpro/src/app/pages/pages.routing.ts
@@ -7,13 +7,12 @@ import { GraficalComponent } from './grafical/grafical.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'dashboard',
     component: PagesComponent,
     children: [
-      {path: 'dashboard', component: DashboardComponent},
+      {path: '', component: DashboardComponent},
       {path: 'progress', component: ProgressComponent},
-      {path: 'grafical', component: GraficalComponent},
-      {path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+      {path: 'grafical', component: GraficalComponent}
     ]
   }
 ];
