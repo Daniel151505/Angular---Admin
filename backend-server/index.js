f3230e4834b14e4f8bb09863694824199f3b2b8b
Daniel151505 <daniel150520011@gmail.com>
2022-07-26 21:43:33 -0500

    Primera ruta

--- backend-server/index.js
+++ backend-server/index.js
@@ -3,6 +3,16 @@ const express = require('express');
 // Crear el servidor de express
 const app = express();
 
-app.listen(3001, () => {
-    console.log('Serivdor corriendo en puerto '+ 3001)
+// Rutas
+app.get( '/', (req, res) => {
+
+    res.json({
+        ok: true,
+        msg: 'Hola Mundo'
+    })
+    
+} );
+
+app.listen(3000, () => {
+    console.log('Serivdor corriendo en puerto '+ 3000)
 })
