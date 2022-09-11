4ac5b1d594730e0ad6a774a5b568fecc0f86888f
Daniel151505 <daniel150520011@gmail.com>
2022-09-10 22:36:48 -0500

    Formulario de Registro - Reactive Forms

--- adminpro/src/app/auth/register/register.component.ts
+++ adminpro/src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public registerForm = this.fb.group({
+    nombre: ['Sara', Validators.required ],
+    email: ['test001@gmail.com', Validators.required ],
+    password: ['123', Validators.required ],
+    password2: ['123', Validators.required ],
+    terminos: [ false, Validators.required ]
+  })
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  crearUsuario() {
+    console.log( this.registerForm.value );
+  }
 
   ngOnInit(): void {
   }
